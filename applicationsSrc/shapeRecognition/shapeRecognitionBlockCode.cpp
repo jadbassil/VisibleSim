d10fc7225116cb6f2efbbcd5e935fcb8649aec23
jadbassil <jadbassil@github.com>
2022-06-20 19:42:37 +0200

    Find currentShape

---
+++ applicationsSrc/shapeRecognition/shapeRecognitionBlockCode.cpp
@@ -0,0 +1,302 @@
+#include "shapeRecognitionBlockCode.hpp"
+
+using namespace Catoms3D;
+
+ShapeRecognitionBlockCode::ShapeRecognitionBlockCode(Catoms3DBlock *host) : Catoms3DBlockCode(host) {
+    // @warning Do not remove block below, as a blockcode with a NULL host might be created
+    //  for command line parsing
+    if (not host) return;
+
+    // Registers a callback (handleSampleMessage) to the message of type SAMPLE_MSG_ID
+    addMessageEventFunc2(SETL_MSG_ID,
+                         std::bind(&ShapeRecognitionBlockCode::handleSetLMessage, this,
+                                   std::placeholders::_1, std::placeholders::_2));
+
+    addMessageEventFunc2(GETW_MSG_ID,
+                         std::bind(&ShapeRecognitionBlockCode::handleGetWMessage, this,
+                                   std::placeholders::_1, std::placeholders::_2));
+
+    addMessageEventFunc2(SETW_MSG_ID,
+                         std::bind(&ShapeRecognitionBlockCode::handleSetWMessage, this,
+                                   std::placeholders::_1, std::placeholders::_2));
+
+    addMessageEventFunc2(CHECKLR_MSG_ID,
+                         std::bind(&ShapeRecognitionBlockCode::handleCheckLRMessage, this,
+                                   std::placeholders::_1, std::placeholders::_2));
+
+    addMessageEventFunc2(SENDLR_MSG_ID,
+                         std::bind(&ShapeRecognitionBlockCode::handleSendLRMessage, this,
+                                   std::placeholders::_1, std::placeholders::_2));
+
+    addMessageEventFunc2(BROADCASTRECT_MSG_ID,
+                         std::bind(&ShapeRecognitionBlockCode::handleBroadcastRectWMessage, this,
+                                   std::placeholders::_1, std::placeholders::_2));
+    // Set the module pointer
+    module = static_cast<Catoms3DBlock*>(hostBlock);
+  }
+
+void ShapeRecognitionBlockCode::startup() {
+    console << "start\n";
+    l=-1;
+    leftL = rightL = -1;
+    if(isTop()) {
+        module->setColor(RED);
+        l = 1;
+        if(lattice->cellHasBlock(module->position.offsetY(-1))) {
+            sendMessage("SetL msg", new MessageOf<int>(SETL_MSG_ID, l),
+                        module->getInterface(lattice->getCellInDirection(module->position, FCCLattice::Direction::C7South)), 100, 0);
+        }
+
+    }
+}
+
+void ShapeRecognitionBlockCode::handleSetLMessage(std::shared_ptr<Message> _msg,
+                                               P2PNetworkInterface* sender) {
+    console << "recv setL from " << sender->getConnectedBlockId() << "\n";
+
+    MessageOf<int>* msg = static_cast<MessageOf<int>*>(_msg.get());
+
+    l = *msg->getData() + 1;
+    while (!waiting.empty()) {
+        sendMessage("SendLR msg", new MessageOf<int>(SENDLR_MSG_ID, l), waiting.front(), 100, 0);
+        waiting.pop();
+    }
+    if(lattice->cellHasBlock(module->position.offsetY(-1))) {
+        sendMessage("SetL msg", new MessageOf<int>(SETL_MSG_ID, l),
+                    module->getInterface(lattice->getCellInDirection(module->position, FCCLattice::Direction::C7South)), 100, 0);
+    } else {
+        console << "Bottom hole reached\n";
+        module->setColor(GREEN);
+
+        //search for the width
+        if(lattice->cellHasBlock(module->position.offsetX(1))) {
+            nbWaitedCheckLR++;
+            sendMessage("CheckLR<right> msg", new Message(CHECKLR_MSG_ID), module->getInterface(module->position.offsetX(1)), 100, 0);
+        }
+        if(lattice->cellHasBlock(module->position.offsetX(-1))) {
+            nbWaitedCheckLR++;
+            sendMessage("CheckLR<left> msg", new Message(CHECKLR_MSG_ID), module->getInterface(module->position.offsetX(-1)), 100, 0);
+        }
+
+    }
+
+}
+
+void ShapeRecognitionBlockCode::handleCheckLRMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender) {
+    console << "recv checkLR from " << sender->getConnectedBlockId() << "\n";
+    if(l == -1) {
+        console << "push\n";
+        waiting.push(sender);
+        return;
+    }
+    else {
+        if(isBottom())
+            sendMessage("sendLR msg", new MessageOf<int>(SENDLR_MSG_ID, l), sender, 100, 0);
+        else
+            sendMessage("sendLR msg", new MessageOf<int>(SENDLR_MSG_ID, -1), sender, 100, 0);
+    }
+}
+
+void ShapeRecognitionBlockCode::handleSendLRMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender) { {
+    MessageOf<int>* msg = static_cast<MessageOf<int>*>(_msg.get());
+    console << "recv sendLR msg from " << sender->getConnectedBlockId() <<  "\n";
+
+    int recvL = *msg->getData();
+    nbWaitedCheckLR--;
+    if(sender == module->getInterface(module->position.offsetX(1))) {
+        rightL = recvL;
+    } else {
+        leftL = recvL;
+    }
+    if(nbWaitedCheckLR == 0) {
+        if(l == rightL and l == leftL) return;
+        if(l != rightL and l != leftL) {
+            myRectangle = Rectangle(module->position, l, 1);
+            module->setColor(BLUE);
+            sendMessageToAllNeighbors("BroadcastRect msg", new MessageOf<Rectangle>(BROADCASTRECT_MSG_ID, myRectangle), 100, 0, 0);
+        }
+        if(l == leftL and rightL != l) {
+            sendMessage("GetW message", new MessageOf<int>(GETW_MSG_ID, 1), module->getInterface(module->position.offsetX(-1)), 100, 0);
+        }
+        if(l == rightL and leftL != l) {
+            sendMessage("GetW message", new MessageOf<int>(GETW_MSG_ID, 1), module->getInterface(module->position.offsetX(1)), 100, 0);
+        }
+    }
+}
+
+}
+
+void ShapeRecognitionBlockCode::handleGetWMessage(std::shared_ptr<Message> _msg,
+                                                  P2PNetworkInterface* sender) {
+    MessageOf<int>* msg = static_cast<MessageOf<int>*>(_msg.get());
+    int recvW = *msg->getData();
+    recvW++;
+    console << "recv GetW<" << recvW << ">  msg from " << sender->getConnectedBlockId() << "\n";
+    if(sender == module->getInterface(module->position.offsetX(1))) { // coming from right
+        if(leftL != l) {
+          /*  myRectangle = Rectangle(module->position, l, recvW);*/
+            sendMessage("SetW message", new MessageOf<int>(SETW_MSG_ID, recvW), module->getInterface(module->position.offsetX(1)), 100, 0);
+
+        } else {
+            sendMessage("GetW message", new MessageOf<int>(GETW_MSG_ID, recvW), module->getInterface(module->position.offsetX(-1)), 100, 0);
+        }
+    } else {
+        if(rightL != l) {
+/*
+            myRectangle = Rectangle(module->position, l, recvW);
+*/
+            sendMessage("SetW message", new MessageOf<int>(SETW_MSG_ID, recvW), module->getInterface(module->position.offsetX(-1)), 100, 0);
+
+        } else {
+            sendMessage("GetW message", new MessageOf<int>(GETW_MSG_ID, recvW), module->getInterface(module->position.offsetX(1)), 100, 0);
+        }
+    }
+
+}
+
+void ShapeRecognitionBlockCode::handleSetWMessage(std::shared_ptr<Message> _msg,
+                                                  P2PNetworkInterface* sender) {
+    MessageOf<int>* msg = static_cast<MessageOf<int>*>(_msg.get());
+    int recvW = *msg->getData();
+    if(sender == module->getInterface(module->position.offsetX(1))) { // coming from right
+        if(leftL != l) {
+            myRectangle = Rectangle(module->position, l, recvW);
+            module->setColor(BLUE);
+            currentShape.insert(myRectangle);
+
+            sendMessageToAllNeighbors("BroadcastRect msg", new MessageOf<Rectangle>(BROADCASTRECT_MSG_ID, myRectangle), 100, 0, 0);
+        } else {
+            sendMessage("SetW message", new MessageOf<int>(SETW_MSG_ID, recvW), module->getInterface(module->position.offsetX(-1)), 100, 0);
+        }
+    } else {
+        if(rightL != l) {
+            myRectangle = Rectangle(module->position, l, -recvW);
+            module->setColor(BLUE);
+            currentShape.insert(myRectangle);
+            sendMessageToAllNeighbors("BroadcastRect msg", new MessageOf<Rectangle>(BROADCASTRECT_MSG_ID, myRectangle), 100, 0, 0);
+        } else {
+            sendMessage("SetW message", new MessageOf<int>(SETW_MSG_ID, recvW), module->getInterface(module->position.offsetX(1)), 100, 0);
+        }
+    }
+}
+
+void ShapeRecognitionBlockCode::handleBroadcastRectWMessage(std::shared_ptr<Message> _msg,
+                                                            P2PNetworkInterface *sender) {
+    MessageOf<Rectangle>* msg = static_cast<MessageOf<Rectangle>*>(_msg.get());
+    Rectangle recvRectangle = *msg->getData();
+    if(find(currentShape.begin(), currentShape.end(), recvRectangle) != currentShape.end() or isInshape(recvRectangle.base)) return;
+    currentShape.insert(recvRectangle);
+    sendMessageToAllNeighbors("BroacastRect msg", new MessageOf<Rectangle>(BROADCASTRECT_MSG_ID, recvRectangle), 100, 0, 1, sender);
+}
+
+bool ShapeRecognitionBlockCode::isInshape(Cell3DPosition &pos) {
+    for(auto &r: currentShape) {
+        if(pos.pt[0] >= r.base.pt[0] and pos.pt[0] <= r.base.pt[0] + r.w -1
+            and pos.pt[1] >= r.base.pt[1] and pos.pt[1] <= r.base.pt[1] + r.l -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void ShapeRecognitionBlockCode::onMotionEnd() {
+    console << " has reached its destination" << "\n";
+
+    // do stuff
+    // ...
+}
+
+void ShapeRecognitionBlockCode::processLocalEvent(EventPtr pev) {
+    std::shared_ptr<Message> message;
+    stringstream info;
+
+    // Do not remove line below
+    Catoms3DBlockCode::processLocalEvent(pev);
+
+    switch (pev->eventType) {
+        case EVENT_ADD_NEIGHBOR: {
+            // Do something when a neighbor is added to an interface of the module
+            break;
+        }
+
+        case EVENT_REMOVE_NEIGHBOR: {
+            // Do something when a neighbor is removed from an interface of the module
+            break;
+        }
+    }
+}
+
+/// ADVANCED BLOCKCODE FUNCTIONS BELOW
+
+void ShapeRecognitionBlockCode::onBlockSelected() {
+    // Debug stuff:
+    cerr << endl << "--- PRINT MODULE " << *module << "---" << endl;
+    cerr << "l: " << l << endl;
+    cerr << "nbWaitedCheck: " << nbWaitedCheckLR << endl;
+    cerr << "myRectangle: " << myRectangle.base << " (" << myRectangle.l << ", " << myRectangle.w << ")\n";
+    cerr << "currentSHape: ";
+    for(auto &r: currentShape) {
+        cerr << r.base <<  " (" << r.l << ", " << r.w << ") ; ";
+    }
+    cerr << endl;
+}
+
+void ShapeRecognitionBlockCode::onAssertTriggered() {
+    console << " has triggered an assert" << "\n";
+
+    // Print debugging some info if needed below
+    // ...
+}
+
+bool ShapeRecognitionBlockCode::parseUserCommandLineArgument(int &argc, char **argv[]) {
+    /* Reading the command line */
+    if ((argc > 0) && ((*argv)[0][0] == '-')) {
+        switch((*argv)[0][1]) {
+
+            // Single character example: -b
+            case 'b':   {
+                cout << "-b option provided" << endl;
+                return true;
+            } break;
+
+            // Composite argument example: --foo 13
+            case '-': {
+                string varg = string((*argv)[0] + 2); // argv[0] without "--"
+                if (varg == string("foo")) { //
+                    int fooArg;
+                    try {
+                        fooArg = stoi((*argv)[1]);
+                        argc--;
+                        (*argv)++;
+                    } catch(std::logic_error&) {
+                        stringstream err;
+                        err << "foo must be an integer. Found foo = " << argv[1] << endl;
+                        throw CLIParsingError(err.str());
+                    }
+
+                    cout << "--foo option provided with value: " << fooArg << endl;
+                } else return false;
+
+                return true;
+            }
+
+            default: cerr << "Unrecognized command line argument: " << (*argv)[0] << endl;
+        }
+    }
+
+    return false;
+}
+
+string ShapeRecognitionBlockCode::onInterfaceDraw() {
+    stringstream trace;
+    trace << "Some value " << 123;
+    return trace.str();
+}
+
+bool ShapeRecognitionBlockCode::isBottom() const {
+    return !lattice->cellHasBlock(module->position.offsetY(-1));
+}
+
+bool ShapeRecognitionBlockCode::isTop() const {
+    return !lattice->cellHasBlock(module->position.offsetY(1));
+}
---
+++ applicationsSrc/shapeRecognition/shapeRecognitionBlockCode.hpp
@@ -0,0 +1,157 @@
+#ifndef ShapeRecognitionBlockCode_H_
+#define ShapeRecognitionBlockCode_H_
+
+#include "robots/catoms3D/catoms3DWorld.h"
+#include "robots/catoms3D/catoms3DBlockCode.h"
+#include <queue>
+#include <set>
+
+static const int SETL_MSG_ID = 1000;
+static const int GETW_MSG_ID = 1001;
+static const int CHECKLR_MSG_ID = 1002;
+static const int SENDLR_MSG_ID = 1003;
+static const int SETW_MSG_ID = 1004;
+static const int BROADCASTRECT_MSG_ID = 1005;
+
+using namespace Catoms3D;
+
+struct Rectangle {
+    Cell3DPosition base;
+    int l, w;
+
+    Rectangle() {};
+    Rectangle(Cell3DPosition _base, int _l,int _w): base(_base), l(_l), w(_w) {};
+
+    bool operator<(const Rectangle& r) const {
+        return (base.pt[0] == r.base.pt[0]) ?  base.pt[1] < r.base.pt[1]:  base.pt[0] < r.base.pt[0];
+    }
+
+    bool operator==(const Rectangle& r) const {
+        return r.base == base and l == r.l and w == r.w;
+    }
+
+
+};
+
+class ShapeRecognitionBlockCode : public Catoms3DBlockCode {
+private:
+    int l;
+    Catoms3DBlock *module;
+    queue<P2PNetworkInterface*> waiting;
+    int nbWaitedCheckLR{0};
+    int leftL;
+    int rightL;
+    Rectangle myRectangle;
+    set<Rectangle> currentShape;
+public :
+    ShapeRecognitionBlockCode(Catoms3DBlock *host);
+    ~ShapeRecognitionBlockCode() {};
+
+    /**
+     * This function is called on startup of the blockCode, it can be used to perform initial
+     *  configuration of the host or this instance of the program.
+     * @note this can be thought of as the main function of the module
+     **/
+    void startup() override;
+
+    bool isBottom() const;
+    bool isTop() const;
+
+    /**
+     * @brief Handler for all events received by the host block
+     * @param pev pointer to the received event
+     */
+    void processLocalEvent(EventPtr pev) override;
+
+    /**
+     * @brief Callback function executed whenever the module finishes a motion
+     */
+    void onMotionEnd() override;
+
+    /**
+     * @brief Sample message handler for this instance of the blockcode
+     * @param _msg Pointer to the message received by the module, requires casting
+     * @param sender Connector of the module that has received the message and that is connected to the sender */
+    void handleSetLMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
+    void handleGetWMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
+    void handleCheckLRMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
+    void handleSendLRMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
+    void handleSetWMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
+    void handleBroadcastRectWMessage(std::shared_ptr<Message> _msg, P2PNetworkInterface *sender);
+    bool isInshape(Cell3DPosition &pos);
+    /// Advanced blockcode handlers below
+
+    /**
+     * @brief Provides the user with a pointer to the configuration file parser, which can be used to read additional user information from it.
+     * @param config : pointer to the TiXmlDocument representing the configuration file, all information related to VisibleSim's core have already been parsed
+     *
+     * Called from BuildingBlock constructor, only once.
+     */
+    void parseUserElements(TiXmlDocument *config) override {}
+
+    /**
+     * @brief Provides the user with a pointer to the configuration file parser, which can be used to read additional user information from each block config. Has to be overriden in the child class.
+     * @param config : pointer to the TiXmlElement representing the block configuration file, all information related to concerned block have already been parsed
+     *
+     */
+    void parseUserBlockElements(TiXmlElement *config) override {}
+
+    /**
+     * User-implemented debug function that gets called when a module is selected in the GUI
+     */
+    void onBlockSelected() override;
+
+    /**
+     * User-implemented debug function that gets called when a VS_ASSERT is triggered
+     * @note call is made from utils::assert_handler()
+     */
+    void onAssertTriggered() override;
+
+    /**
+     * User-implemented keyboard handler function that gets called when
+     *  a key press event could not be caught by openglViewer
+     * @param c key that was pressed (see openglViewer.cpp)
+     * @param x location of the pointer on the x axis
+     * @param y location of the pointer on the y axis
+     * @note call is made from GlutContext::keyboardFunc (openglViewer.h)
+     */
+    void onUserKeyPressed(unsigned char c, int x, int y) override {}
+
+    /**
+     * Call by world during GL drawing phase, can be used by a user
+     *  to draw custom Gl content into the simulated world
+     * @note call is made from World::GlDraw
+     */
+    void onGlDraw() override {}
+
+    /**
+     * @brief This function is called when a module is tapped by the user. Prints a message to the console by default.
+     Can be overloaded in the user blockCode
+     * @param face face that has been tapped */
+    void onTap(int face) override {}
+
+
+    /**
+     * User-implemented keyboard handler function that gets called when
+     *  a key press event could not be caught by openglViewer
+     * @note call is made from GlutContext::keyboardFunc (openglViewer.h)
+     */
+    bool parseUserCommandLineArgument(int& argc, char **argv[]) override;
+
+    /**
+     * Called by openglviewer during interface drawing phase, can be used by a user
+     *  to draw a custom Gl string onto the bottom-left corner of the GUI
+     * @note call is made from OpenGlViewer::drawFunc
+     * @return a string (can be multi-line with `\n`) to display on the GUI
+     */
+    string onInterfaceDraw() override;
+
+/*****************************************************************************/
+/** needed to associate code to module                                      **/
+    static BlockCode *buildNewBlockCode(BuildingBlock *host) {
+        return (new ShapeRecognitionBlockCode((Catoms3DBlock*)host));
+    };
+/*****************************************************************************/
+};
+
+#endif /* ShapeRecognitionBlockCode_H_ */
